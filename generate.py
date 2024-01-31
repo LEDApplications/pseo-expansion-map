b04f3663942aaf55c26bdfc4ae3824babc2f9ab8
Jody Hoon-Starr <jody@protonmail.com>
2024-01-31 17:39:01 -0500

    feat: load sqlite db

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@
 import re
 import argparse
 import logging
+import tempfile
 from ppm.log import (
     LOG_FORMAT,
     log_levels,
@@ -10,7 +11,7 @@ from ppm.log import (
 )
 from ppm.download import get_links_in_table, download_url_to_file
 from ppm.zip import ungz
-from ppm.db import get_header_row, create_db_table_if_not_exists, insert_csv_into_db
+from ppm.db import get_header_row, init_db_table, insert_csv_into_db
 
 logger = logging.getLogger(__name__)
 
@@ -53,32 +54,44 @@ def main():
     vintages = [
         item.rstrip("/") for item in links if re.match(r"^R\d{4}Q[1-4]/$", item)
     ]
-    all_file_links = [
-        ALL_FILE_URL_FORMAT.replace("VINTAGE", vintage) for vintage in vintages
-    ]
-
-    # TODO use actual temp dir
-    temp_dir = "./tmp"
-
-    # TODO rm hardcode
-    # shape_zip = download_url_to_file(all_file_links[0], temp_dir)
-    csv_gz = "/home/jody/workspace/language/py/pseo-partners-map/tmp/pseoe_all.csv.gz"
-
-    # TODO rme hardcode
-    # csv = ungz(csv_gz)
-    csv = "/home/jody/workspace/language/py/pseo-partners-map/tmp/pseoe_all.csv"
-
-    # TODO validate that each csv has the same header row
-    header_row = get_header_row(csv)
-
-    # prepend the vintage to the header row
-    header_row = ["vintage"] + header_row
+    url_vintage_list = zip(
+        [ALL_FILE_URL_FORMAT.replace("VINTAGE", vintage) for vintage in vintages],
+        vintages,
+    )
 
     db_path = "./tmp/pseo.db"
     table_name = "pseoe_public"
-    create_db_table_if_not_exists(db_path, table_name, header_row)
-    # TODO fix vintage hardcode
-    insert_csv_into_db(csv, db_path, table_name, "R2020Q4")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # You can use 'temp_dir' here. It will be automatically deleted when the 'with' block ends.
+
+        logger.debug(f"temp directory path is {temp_dir}")
+
+        # download and extract all the csv files
+        # since we'll need to unify the columns since they change over time
+        csv_vintage_list = []
+        for url, vintage in url_vintage_list:
+            logger.info(f"loading {vintage} data")
+
+            destination_file_name = f"pseoe_all_{vintage}.csv.gz"
+            csv_gz = download_url_to_file(url, temp_dir, destination_file_name)
+            csv = ungz(csv_gz)
+            csv_vintage_list.append((csv, vintage))
+
+        # unify the headers
+        unified_header = set()
+        for csv, _ in csv_vintage_list:
+            header_row = get_header_row(csv)
+            unified_header.update(header_row)
+
+        # create the db table and schema with an added "vintage" column
+        header_row = ["vintage"] + list(unified_header)
+        init_db_table(db_path, table_name, header_row)
+
+        for csv, vintage in csv_vintage_list:
+            logger.info(f"inserting {vintage} into {db_path}")
+            insert_csv_into_db(csv, db_path, table_name, vintage)
+
+    logger.info("done.")
 
 
 if __name__ == "__main__":
--- ppm/db.py
+++ ppm/db.py
@@ -17,7 +17,7 @@ def insert_csv_into_db(csv_file_path: str, db_path: str, table_name: str, vintag
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
 
-    with open(csv_file_path, "r") as csv_file:
+    with open(csv_file_path, "r", encoding="utf-8") as csv_file:
         reader = csv.reader(csv_file)
         header_row = next(reader)
         header = ["vintage"] + header_row
@@ -35,15 +35,27 @@ def insert_csv_into_db(csv_file_path: str, db_path: str, table_name: str, vintag
 
 
 def get_header_row(csv_file_path: str):
-    with open(csv_file_path, "r") as csv_file:
+    with open(csv_file_path, "r", encoding="utf-8") as csv_file:
         reader = csv.reader(csv_file)
         header_row = next(reader)
 
     return header_row
 
 
-def create_db_table_if_not_exists(db_path: str, table_name: str, columns: list):
-    """Create a sqlite db of with a single table and text columns
+def truncate_table(db_path: str, table_name: str):
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM {table_name}")
+    conn.commit()
+    conn.close()
+    logger.debug(f"truncated table {table_name} in {db_path}")
+
+
+def init_db_table(db_path: str, table_name: str, columns: list):
+    """
+        Create a sqlite db of with a single table and text columns
+        if it already exists, truncate the table
+
 
     Args:
         db_path (str): path to the sqlite db
@@ -62,3 +74,4 @@ def create_db_table_if_not_exists(db_path: str, table_name: str, columns: list):
         logger.debug(f"created db at {db_path}")
     else:
         logger.debug(f"db at {db_path} already exists")
+        truncate_table(db_path, table_name)
